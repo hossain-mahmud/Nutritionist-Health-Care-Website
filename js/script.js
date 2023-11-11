9d5ca5f4dd73e86321c6c0fdf3457ebdd479fda5
hossain <hossainmahmud.cse@gmail.com>
2023-11-11 18:02:11 +0600

    menu fixed end

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // aos animation
 AOS.init();
+
+// nav menu fixed
+let mainMenu=document.querySelector('.main_menu');
+window.addEventListener('scroll',()=>{
+    let scrooling=this.scrollY;
+    if(scrooling >150){
+        mainMenu.classList.add('menu_fixed');
+    }
+    else{
+        mainMenu.classList.remove('menu_fixed');
+    }
+})
