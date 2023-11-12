1b6576e74cb73c3866ebd80b15ebbccceb8b39b1
hossain <hossainmahmud.cse@gmail.com>
2023-11-12 10:15:36 +0600

    add pricing section

--- js/script.js
+++ js/script.js
@@ -47,3 +47,40 @@ $('.testimonial_items').slick({
         }
       ]
 });
+
+
+
+$('.nav_slider_items').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    prevArrow: '<i class="fa-solid fa-chevron-left prev_arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right next_arrow"></i>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
